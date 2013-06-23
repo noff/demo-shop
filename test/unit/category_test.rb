5e630e0d5e62864a373925b15337f528a60cdc13
Michael Kechinov <kechinoff@gmail.com>
2013-06-23 18:38:43 +0400

    Добавил тест.

--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "Category Creation" do
+		category = Category.new
+		category.save
+		assert_nil category.id
+
+		category.name = "Something"
+		category.save
+		assert_not_nil category.id
+
+	end
+
 end
